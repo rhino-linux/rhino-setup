7ac4a8820108683e2357600926b31e4fd7fe8823
oklopfer <104327997+oklopfer@users.noreply.github.com>
2023-02-04 12:14:19 -0500

    Update progress.rs

--- src/progress.rs
+++ src/progress.rs
@@ -199,7 +199,7 @@ impl SimpleComponent for ProgressModel {
                     if !error_occured {
                         sender.output(Self::Output::InstallationComplete);
                         tracing::info!("Installation complete");
-                        Command::new("sh").arg("sudo rm -f /usr/local/bin/rhino-setup").status().unwrap();
+                        Command::new("sh").arg("set -e sudo rm -f /usr/local/bin/rhino-setup").status().unwrap();
                     }
                 });
             },
