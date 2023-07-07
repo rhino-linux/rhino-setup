8aa1f7920fc8481e77e0c161bd6412b9037fdb04
oklopfer <104327997+oklopfer@users.noreply.github.com>
2023-07-07 22:48:17 +0000

    attempt to fix pkexec

--- src/done.rs
+++ src/done.rs
@@ -101,7 +101,7 @@ impl SimpleComponent for DoneModel {
         match message {
             Self::Input::Reboot => {
                 Command::new("pkexec")
-                    .args(["sh", "-c", "reboot"])
+                    .arg("/sbin/reboot")
                     .status()
                     .unwrap();
             },
